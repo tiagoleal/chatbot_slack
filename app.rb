b31ff05d12891acf927612764a4649d529a46b49
Tiago Leal <tiago.sleal12@gmail.com>
2019-01-20 14:10:21 -0200

    create models

--- app.rb
+++ app.rb
@@ -1,8 +1,10 @@
 require 'json'
 require 'sinatra'
 require 'sinatra/activerecord'
-
 require './config/database'
+
+# Load Models
+Dir["./app/models/*.rb"].each {|file| require file }
 
 class App < Sinatra::Base
   get '/' do
