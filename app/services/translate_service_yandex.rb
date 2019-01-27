0f3ba136df3eb2a019af7fa73af76349c993c263
Tiago Leal <tiago.sleal12@gmail.com>
2019-01-27 14:17:44 -0200

    create and validate the integration with Yandex translate web service

---
+++ app/services/translate_service_yandex.rb
@@ -0,0 +1,28 @@
+require 'rest-client'
+require 'json'
+
+class TranslateServiceYandex
+  def initialize(lang_src,lang_dst,text)
+    @language = "#{lang_src}-#{lang_dst}"
+    @text = text    
+    @key = "trnsl.1.1.20190119T104230Z.77f633d2d53a0031.ec4be61e8296f3ce425c154283e00ddaae58a1f8" 
+  end
+
+  def translate
+    begin
+      translate_api_url = "https://translate.yandex.net/api/v1.5/tr.json/translate?"
+      url_yandex = "#{translate_api_url}key=#{@key}&lang=#{@language}"
+
+      # res = RestClient.post url_yandex, text: @text.to_json, :accept => :json 
+      res = RestClient.post(url_yandex, {text: @text})
+      value = JSON.parse(res.body)["text"].to_s
+
+      # r = RestClient.post('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20190119T104230Z.77f633d2d53a0031.ec4be61e8296f3ce425c154283e00ddaae58a1f8&lang=pt-en', {text: 'ola'})
+      # r = RestClient.post('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20190119T104230Z.77f633d2d53a0031.ec4be61e8296f3ce425c154283e00ddaae58a1f8&lang=pt-en', {text: 'ola'})
+      
+
+    rescue RestClient::ExceptionWithResponse => e
+      e.response
+    end
+  end
+end
---
+++ spec/services/translate_service_yandex_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+require './app/services/translate_service_yandex'
+
+describe "translate message" do
+  before do 
+    @description_pt = "Olá mundo!" 
+    @description_en = "hello World!" 
+  end
+
+  it "translate pt-br to eng" do
+    response = TranslateServiceYandex.new("pt","en",@description_pt).translate
+    puts "Retorno pt-en => "+response
+    expect(response.is_a? String).to eql(true)
+  end
+
+  it "translate eng to pt" do
+    response = TranslateServiceYandex.new("en","pt",@description_en).translate
+    puts "Retorno en-pt => "+response
+    expect(response.is_a? String).to eql(true)
+  end
+
+end
