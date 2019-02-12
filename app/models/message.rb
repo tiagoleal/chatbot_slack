be750a8a01558824c533aae7fb0fcddf114651ca
Tiago Leal <tiago.sleal12@gmail.com>
2019-02-12 21:55:05 -0200

    create the first stable version of the chatbot

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,6 @@ require "pg_search"
 
 class Message < ActiveRecord::Base
   include PgSearch
-
   validates_presence_of :description, :language
   pg_search_scope :search, :against => [:description, :language]
 end
